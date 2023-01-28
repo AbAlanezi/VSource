ad079c2d6a306879646263100e8d890d1911398b
KhaledAlfawaz <khaled@al-fawaz.net>
2023-01-28 04:54:19 +0300

    added new navbar and update index.html

--- index.js
+++ index.js
@@ -79,3 +79,22 @@ function cardsGroup() {
     
   }))
 }
+
+const nav = document.querySelector("nav");
+const btnToggleNav = document.querySelector(".hamburger-menu");
+const form = document.querySelector(".hero-gauche form");
+
+// navbar responsive
+btnToggleNav.addEventListener('click', () => {
+
+  nav.classList.toggle("active");
+  btnToggleNav.classList.toggle("active");
+
+})
+
+// Le prevent default sur le formulaire
+form.addEventListener("submit", (e) => {
+
+  e.preventDefault();
+
+})
